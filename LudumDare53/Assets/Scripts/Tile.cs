45d1a03c79ae22a6e23e09d40ec5d12e02046aff
Dan <mrdanjh@gmail.com>
2023-04-29 18:26:46 +0100

    Randomly activate some towers on each tile

--- LudumDare53/Assets/Scripts/Tile.cs
+++ LudumDare53/Assets/Scripts/Tile.cs
@@ -6,8 +6,21 @@ public class Tile : MonoBehaviour
 {
     public Transform m_waveOffset;
 
+    public List<Tower> m_possibleTargetTowers = new List<Tower>();
+    
+    public List<Tower> TargetTowers { get; } = new List<Tower>();
+
     public void Activate(bool activateWave)
     {
+        TargetTowers.Clear();
+        TargetTowers.AddRange(m_possibleTargetTowers);
+
+        while(TargetTowers.Count > 2)
+            TargetTowers.RemoveAt(Random.Range(0, TargetTowers.Count));
+
+        foreach(var tower in m_possibleTargetTowers)
+            tower.Activate(TargetTowers.Contains(tower));
+
         foreach(Transform child in m_waveOffset)
             child.gameObject.SetActive(activateWave);
             
