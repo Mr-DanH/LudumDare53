29482b5f668c2fe78db20a830cf69ee58d951df5
Dan <mrdanjh@gmail.com>
2023-04-29 13:03:43 +0100

    Randomised tiles, add singleton class

--- LudumDare53/Assets/Scripts/ScrollingLevel.cs
+++ LudumDare53/Assets/Scripts/ScrollingLevel.cs
@@ -1,34 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class ScrollingLevel : MonoBehaviour
+public class ScrollingLevel : Singleton<ScrollingLevel>
 {
     public float m_speed = 1;
     public float m_spacing = 10;
     public float m_wrapAroundZ;
+    public int m_numActiveTiles = 2;
     
-    List<Transform> m_tiles = new List<Transform>();
+    List<Transform> m_tilePool = new List<Transform>();
+    List<Transform> m_activeTiles = new List<Transform>();
 
-    void Awake()
+    public override void Awake()
     {
-        foreach(Transform child in transform)
-            m_tiles.Add(child);
+        base.Awake();
 
-        for(int i = 0; i < m_tiles.Count; ++i)
-            m_tiles[i].position = Vector3.forward * i * m_spacing;
+        foreach(Transform child in transform)
+        {
+            m_tilePool.Add(child);
+            child.gameObject.SetActive(false);
+        }
+
+        for(int i = 0; i < m_numActiveTiles; ++i)
+            AddTileToEnd();
+    }
+
+    void AddTileToEnd()
+    {
+        int index = Random.Range(0, m_tilePool.Count);
+        Transform tile = m_tilePool[index];
+
+        if(m_activeTiles.Count == 0)
+            tile.position = Vector3.zero;
+        else
+            tile.position = m_activeTiles.Last().position + (Vector3.forward * m_spacing);
+
+        tile.gameObject.SetActive(true);
+
+        m_activeTiles.Add(tile);
+        m_tilePool.RemoveAt(index);
     }
 
     void Update()
     {
         Vector3 shift = Vector3.back * Time.deltaTime * m_speed;
 
-        foreach(var tile in m_tiles)
+        foreach(var tile in m_activeTiles)
         {
             tile.position += shift;
+        }
 
-            if(tile.position.z < m_wrapAroundZ)
-                tile.position += Vector3.forward * m_tiles.Count * m_spacing;            
+        if(m_activeTiles[0].position.z < m_wrapAroundZ)
+        {
+            AddTileToEnd();
+
+            m_activeTiles[0].gameObject.SetActive(false);
+            
+            m_tilePool.Add(m_activeTiles[0]);
+            m_activeTiles.RemoveAt(0);
         }
     } 
 }
