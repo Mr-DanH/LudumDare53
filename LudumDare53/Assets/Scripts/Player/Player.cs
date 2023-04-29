fe99a5a7a4baa0e553316df1d6fb9effa51cf65d
Samantha Keane <sam.l.keane@gmail.com>
2023-04-29 15:36:23 +0100

    Player Movement

--- LudumDare53/Assets/Scripts/Player/Player.cs
+++ LudumDare53/Assets/Scripts/Player/Player.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class Player : Singleton<Player>
 {
     // todo - pigeon manager
+    [SerializeField] private float baseSpeed;
 
-    public void UpdateMovement(Vector2 movement)
+    private Vector3 currentDirection = new Vector3();
+    private RectTransform rect;
+
+    public override void Awake()
     {
-        
+        base.Awake();
+        rect = transform as RectTransform;
+    }
+
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    public void SetMovementDirection(Vector2 movement)
+    {
+        currentDirection = movement;
     }
 
     public void Fire()
@@ -24,5 +39,26 @@ public class Player : Singleton<Player>
     public void FireLeft()
     {
 
+    }
+
+    private void UpdateMovement()
+    {
+        Vector3 newPosition = rect.localPosition + (currentDirection * baseSpeed);
+        bool clamp = false;
+        int screenY = Screen.height/2;
+        int screenX = Screen.width/2;
+        bool isTooHigh = rect.rect.yMin > screenY;
+        bool isTooLow = rect.rect.yMax < -screenY;
+        bool isTooRight = rect.rect.xMin > screenX;
+        bool isTooLeft = rect.rect.xMax < -screenX;
+        clamp = isTooHigh || isTooLow || isTooRight || isTooLeft;
+
+        if (clamp)
+        {
+            newPosition.x = Mathf.Clamp(newPosition.x, -screenX, screenX);
+            newPosition.y = Mathf.Clamp(newPosition.y, -screenY, screenY);
+        }
+
+        rect.localPosition = newPosition;
     }
 }
